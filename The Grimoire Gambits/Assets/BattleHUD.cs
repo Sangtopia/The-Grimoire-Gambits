cae1d3b7ae91eeb7c5ca19e94b034bd02091adb2
Khai <thachqkhai@gmail.com>
2023-12-06 23:47:51 -0500

    idek

--- The Grimoire Gambits/Assets/BattleHUD.cs
+++ The Grimoire Gambits/Assets/BattleHUD.cs
@@ -8,7 +8,7 @@ public class BattleHUD : MonoBehaviour
     public Text nameText;
     public Text levelText;
     public Slider hpSlider;
-
+    public int maxHP = 100;
     public void SetHUD(Unit unit)
     {
         nameText.text = unit.unitName;
