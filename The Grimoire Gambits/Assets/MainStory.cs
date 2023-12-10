1878e59c1a64204d9a6eba77055d00d4449dafa2
Sangtopia <emsa627@gmail.com>
2023-12-10 00:17:21 -0500

    Fixed Cut Scene

--- The Grimoire Gambits/Assets/MainStory.cs
+++ The Grimoire Gambits/Assets/MainStory.cs
@@ -8,6 +8,6 @@ public class MainStory : MonoBehaviour
     // Start is called before the first frame update
     void OnEnable() // Corrected method name
     {
-        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        SceneManager.LoadScene("Villiage", LoadSceneMode.Single);
     }
 }
